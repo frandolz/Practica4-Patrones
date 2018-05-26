4a7f0d9d52828fda2d4af1f74eaa548405fa8160
Oriol Queralt Amorós <jqa1@alumnes.udl.cat>
2018-05-26 18:52:27 +0200

    Implementando AreaDoubler apartado 3

---
+++ Practica4-Patrones/src/AreaDoubler.java
@@ -0,0 +1,23 @@
+/**
+ * @author F.Dolz
+ * @author Oriol
+ * @author Alberto
+ */
+public class AreaDoubler implements FigureVisitor{
+
+    @Override
+    public void visit(Circle circle) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void visit(Rectangle rectangle) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void visit(Drawing drawing) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
